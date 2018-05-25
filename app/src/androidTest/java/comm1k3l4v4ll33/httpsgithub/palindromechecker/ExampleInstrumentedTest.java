6cfbc555656d85e86d86e724a6735d183a256b8e
M1K3L4V4LL33 <36162644+M1K3L4V4LL33@users.noreply.github.com>
2018-05-25 12:17:32 -0500

    Initial commit

---
+++ app/src/androidTest/java/comm1k3l4v4ll33/httpsgithub/palindromechecker/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package comm1k3l4v4ll33.httpsgithub.palindromechecker;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("comm1k3l4v4ll33.httpsgithub.palindromechecker", appContext.getPackageName());
+    }
+}
